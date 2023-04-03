a1e5e56954a86f40bda34b30e6f839670c9202b3
Alberto Paganelli <albi1600@gmail.com>
2023-04-03 10:53:57 +0200

    red test for zip right

--- src/test/scala/u05lab/ex1/ListTest.scala
+++ src/test/scala/u05lab/ex1/ListTest.scala
@@ -1,11 +1,15 @@
 package u05lab.ex1
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ListTest {
+  import u05lab.ex1.List.*
 
   /** todo */
   @Test
-  def testZipRight(): Unit = {}
-
+  def testZipRight(): Unit =
+    val l = 10 :: 20 :: 30 :: Nil()
+    assertEquals((10, 0) :: (20, 0) :: (30, 0) :: Nil(), l.zipRight)
+    //assertEquals(Cons("10", Cons("20", Cons("30", Nil()))), map(l)(_ + ""))
 }
